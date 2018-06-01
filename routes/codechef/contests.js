c30c0d4c8f0bcb19a38028f5215936118309b4cd
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2018-06-01 18:22:11 +0530

    codechef division system added

--- routes/codechef/contests.js
+++ routes/codechef/contests.js
@@ -47,6 +47,14 @@ function getContests(table) {
     tableParser($);
     var data = $("table").parsetable(false, false, true);
     var ret = [];
+    /*
+     * Codechef introduced rating system on 2nd may 2018
+     * Therefore all contests before that date will have only one link
+     * All contests after 2nd may 2018 will have 2 links one for div - A and one for div - B
+     * 1st may in unix format is 1519896600
+     * So if time is less than above unix time it will have only one link
+     * Otherwise it will have two links div A and div B
+     */
     for (var i = 1; i < data[0].length; i++) {
         var json = {};
         json['code'] = data[0][i];
@@ -54,6 +62,16 @@ function getContests(table) {
         json['start'] = config.normal_to_unix(data[2][i]);
         json['end'] = config.normal_to_unix(data[3][i]);
         json['duration'] = json['end'] - json['start'];
+        json['divA_link'] = json['start'] < 1519896600 ? "" : urls.codechef_contest_link.replace("contest_code", json['code'] + "A");
+        json['divB_link'] = json['start'] < 1519896600 ? "" : urls.codechef_contest_link.replace("contest_code", json['code'] + "B");
+        json['normal_link'] = urls.codechef_contest_link.replace("contest_code", json['code']);
+        /*
+
+        Normal link is given to all even though they are with rating
+        This is to avoid invalid page links for non-codechef contests
+        Normal page link is valid even for divisioned contests
+
+         */
         ret.push(json);
     }
     return ret;
--- urls.js
+++ urls.js
@@ -8,7 +8,10 @@ var json = {
     hackerrank_contests: "https://www.hackerrank.com/contests",
     hackerrank_leaderboard: "https://www.hackerrank.com/contests/contest_id/leaderboard/page_no",
     hackerrank_profile: "https://www.hackerrank.com/username?hr_r=1",
+
+    //codechef contests
     codechef_contests: "https://www.codechef.com/contests",
+    codechef_contest_link:"https://www.codechef.com/contest_code",
     codechef_leaderboard: "https://www.codechef.com/rankings/contest_id?&page=page_no",
     codechef_profile: "https://www.codechef.com/users/user_name",
     codechef_header: {
@@ -20,6 +23,8 @@ var json = {
         "Upgrade-Insecure-Requests": 1,
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0"
     },
+
+
     codeforces_contests:"http://codeforces.com/api/contest.list"
 };
 module.exports = json;
